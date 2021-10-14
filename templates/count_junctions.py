402b3f3dfea0ec287ecc5a45fcc4bf4c4d98a8d3
Sam Minot <sminot@fredhutch.org>
2021-10-14 13:44:43 -0700

    Account for specimens in which no junctions are found

--- templates/count_junctions.py
+++ templates/count_junctions.py
@@ -27,12 +27,30 @@ assert os.path.exists(contigs_fp)
 target_fp = "target.json.gz"
 assert os.path.exists(target_fp)
 
+# Set up the path where the output data will be written to
+output_fp = "junctions.csv.gz"
+
 def read_json(fp):
+    """Read a compressed JSON as a DataFrame."""
+
+    # Make sure the file exists
     assert os.path.exists(fp)
+
+    # Open the file for reading
     with gzip.open(fp, "rt") as handle:
-        return pd.DataFrame(
-            json.load(handle)
-        )
+
+        # Read in the data as JSON
+        dat = json.load(handle)
+
+    # If there isn't any data
+    if len(dat) == 0:
+
+        return None
+
+    else:
+
+        # Format as a DataFrame
+        return pd.DataFrame(dat)
 
 # For each set of alignments, calculate the distance to
 # the end of the contig that it is pointing towards, and
@@ -133,65 +151,88 @@ def calc_positional_offset(target_aln, contig_aln):
             # on the linked contig, minus the length of the linked contig
             return target_start - insert_length + linked_start - linked_length
 
-# Read in the data and process it
+
+def summarize_junctions(contigs_df, target_df, output_fp):
+    """
+    Process the information on mappings to both the contigs and the target,
+    writing out the information to output_fp
+    """
+
+    # Keep a list of junctions
+    junction_list = list()
+
+    # Iterate over each read which aligned to the target
+    for read_name, target_aln in target_df.set_index("query_name").iterrows():
+
+        # Find the pair for this read in the contig alignment
+        ix = (contigs_df['query_name'] == read_name)
+
+        # If the other half of this read did not align
+        if ix.sum() == 0:
+            
+            # Skip it
+            continue
+
+        # Iterate over each alignment position across the contigs
+        for _, contig_aln in contigs_df.loc[ix].iterrows():
+
+            # Figure out the relative orientation of the linked contig
+            orientation = 'fwd' if target_aln['strand'] == contig_aln['strand'] else 'rev'
+
+            # Figure out the positional offset of the linked contig
+            pos_offset = calc_positional_offset(target_aln, contig_aln)
+
+            # Keep track of the data for this junction
+            junction_list.append(
+                dict(
+                    # The direction of the linkage is given by the strand of the
+                    # read aligning to the target contig
+                    direction=target_aln['strand'],
+                    # The relative orientation of the linked contig
+                    orientation=orientation,
+                    # Position of the read in the target contig
+                    target_position=target_aln['position'],
+                    # First aligned base of the fragment on the target contig
+                    target_start=target_aln['reference_start'],
+                    # Position of the read in the linked contig
+                    linked_position=contig_aln['position'],
+                    # First aligned base of the fragment on the linked contig
+                    linked_start=contig_aln['reference_start'],
+                    # The ID of the contig that's linked
+                    linked_contig=contig_aln["reference_name"],
+                    # The length of the linked contig
+                    linked_contig_len=contig_aln["reference_length"],
+                    # The length of the target contig
+                    target_contig_len=target_aln["reference_length"],
+                    # The ID of the read which supports this junction
+                    read_name=read_name,
+                    # Positional offset
+                    pos_offset=pos_offset
+                )
+            )
+
+    # Make a DataFrame with the results
+    junction_df = pd.DataFrame(junction_list)
+
+    # Write to a file
+    junction_df.to_csv(output_fp, index=None)
+
+# Read in the data
+print("Classifying the junction position end for the contigs")
 contigs_df = classify_junction_position(read_json(contigs_fp))
+print("Classifying the junction position end for the target")
 target_df = classify_junction_position(read_json(target_fp))
 
-# Keep a list of junctions
-junction_list = list()
+# If there are no reads aligning to either the target or the contigs
+if contigs_df.shape[0] == 0 or target_df.shape[0] == 0:
 
-# Iterate over each read which aligned to the target
-for read_name, target_aln in target_df.set_index("query_name").iterrows():
+    # Then there is no junction information,
+    # so just write out an empty file
+    with gzip.open(output_fp, "wt") as handle:
+        pass
 
-    # Find the pair for this read in the contig alignment
-    ix = (contigs_df['query_name'] == read_name)
+# Otherwise, if there is junction information
+else:
 
-    # If the other half of this read did not align
-    if ix.sum() == 0:
-        
-        # Skip it
-        continue
-
-    # Iterate over each alignment position across the contigs
-    for _, contig_aln in contigs_df.loc[ix].iterrows():
-
-        # Figure out the relative orientation of the linked contig
-        orientation = 'fwd' if target_aln['strand'] == contig_aln['strand'] else 'rev'
-
-        # Figure out the positional offset of the linked contig
-        pos_offset = calc_positional_offset(target_aln, contig_aln)
-
-        # Keep track of the data for this junction
-        junction_list.append(
-            dict(
-                # The direction of the linkage is given by the strand of the
-                # read aligning to the target contig
-                direction=target_aln['strand'],
-                # The relative orientation of the linked contig
-                orientation=orientation,
-                # Position of the read in the target contig
-                target_position=target_aln['position'],
-                # First aligned base of the fragment on the target contig
-                target_start=target_aln['reference_start'],
-                # Position of the read in the linked contig
-                linked_position=contig_aln['position'],
-                # First aligned base of the fragment on the linked contig
-                linked_start=contig_aln['reference_start'],
-                # The ID of the contig that's linked
-                linked_contig=contig_aln["reference_name"],
-                # The length of the linked contig
-                linked_contig_len=contig_aln["reference_length"],
-                # The length of the target contig
-                target_contig_len=target_aln["reference_length"],
-                # The ID of the read which supports this junction
-                read_name=read_name,
-                # Positional offset
-                pos_offset=pos_offset
-            )
-        )
-
-# Make a DataFrame with the results
-junction_df = pd.DataFrame(junction_list)
-
-# Write to a file
-junction_df.to_csv("junctions.csv.gz", index=None)
+    # Write out a table with all of the junction information
+    summarize_junctions(contigs_df, target_df, output_fp)
